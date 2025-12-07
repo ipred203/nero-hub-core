e483eb110db5bfff329bb93c5070c6165cc2dad7
ipred203 <relife.dobr@gmail.com>
2025-12-07 18:40:25 +0400

    fix init

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify
+from config import Config
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    return jsonify({"status": "Nero Hub is running", "mode": "Dev"})
+    return jsonify({
+        "status": f"{Config.APP_NAME} is running",
+        "mode": "Dev"
+    })
